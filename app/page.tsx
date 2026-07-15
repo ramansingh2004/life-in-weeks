f7d639279a8f2aa481279ca533dc8902953f7c5f
ramansingh2004 <ramansingh29112004@gmail.com>
2026-07-15 13:23:47 +0530

    toggleflow testing

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { isFeatureEnabled } from '@/lib/toggleflow';
  * permanently capturing its value during next build.
  */
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default async function Page() {
   const LoginModeEnabled =
